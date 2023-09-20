5d1c727ae69859555df79aa382bcc3dc4734610f
cccoven <stayhunger0201@gmail.com>
2023-09-20 23:42:26 +0800

    run

--- sentinel.go
+++ sentinel.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 	"log"
 	"net"
+	"time"
 )
 
 type SIFlags uint
@@ -57,11 +61,14 @@ func NewSentinelAddr(host string, port int) *SentinelAddr {
 }
 
 type SentinelInstance struct {
-	flags SIFlags
-	name  string
-	addr  *SentinelAddr
+	client *redis.Client
+	flags  SIFlags
+	name   string
+	addr   *SentinelAddr
 
-	quorum int
+	quorum    int
+	Sentinels map[string]*SentinelInstance
+	Slaves    map[string]*SentinelInstance
 }
 
 func NewSentinelInstance(name string, flags SIFlags, host string, port int, quorum int, master *SentinelInstance) *SentinelInstance {
@@ -70,12 +77,43 @@ func NewSentinelInstance(name string, flags SIFlags, host string, port int, quor
 	inst.name = name
 	inst.addr = NewSentinelAddr(host, port)
 	inst.quorum = quorum
+	inst.Sentinels = make(map[string]*SentinelInstance)
+	inst.Slaves = make(map[string]*SentinelInstance)
+
+	inst.client = redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%d", inst.addr.IP, inst.addr.Port),
+	})
+
+	if flags&SIMaster != 0 {
+		Sentinel.Masters[inst.name] = inst
+	} else if flags&SISlave != 0 {
+		inst.Slaves[inst.name] = inst
+	} else if flags&SISentinel != 0 {
+		inst.Sentinels[inst.name] = inst
+	}
 
 	return inst
 }
 
 func (s *SentinelStat) Run() {
+	t := time.NewTicker(time.Second)
+	for _ = range t.C {
+		s.run()
+	}
+}
 
+func (s *SentinelStat) run() {
+	s.HandleInstances(s.Masters)
+}
+
+func (s *SentinelStat) HandleInstances(instances map[string]*SentinelInstance) {
+	for _, inst := range instances {
+		inst.Handle()
+		if inst.flags&SIMaster != 0 {
+			s.HandleInstances(inst.Slaves)
+			s.HandleInstances(inst.Sentinels)
+		}
+	}
 }
 
 func (s *SentinelStat) ParseConfig() error {
@@ -109,4 +147,23 @@ func (s *SentinelStat) HandleConfiguration() error {
 	)
 
 	return nil
+}
+
+func (si *SentinelInstance) Handle() {
+	si.SendPeriodCommands()
+}
+
+func (si *SentinelInstance) SendPeriodCommands() {
+	if si.flags&SISentinel == 0 {
+		infoResult, _ := si.client.Info(context.Background()).Result()
+		fmt.Println(infoResult)
+	}
+}
+
+func (si *SentinelInstance) CheckSubjectivelyDown() {
+
+}
+
+func (si *SentinelInstance) CheckObjectivelyDown() {
+
 }
